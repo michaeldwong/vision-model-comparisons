dad4cbe33a659f72e64b2d7e3dbfa28e15742532
Mike Wong <michael.d.wong362@gmail.com>
2023-07-17 10:54:56 -0700

    Add log_energy

--- log_energy.sh
+++ log_energy.sh
@@ -7,5 +7,17 @@ do
 #    nvidia-smi -i 0 -q -d power >> energy_log.txt
     nvidia-smi >> energy_log.txt
 done
-
+#echo "" > energy_log.txt
+#echo "" > temperature_log.txt 
+#echo "" > clock_log.txt
+#while [ true ]
+#do
+#    date >> energy_log.txt 
+#    date >> temperature_log.txt
+#    date >> clock_log.txt
+#    nvidia-smi >> energy_log.txt 
+#    nvidia-smi -q -d temperature >> temperature_log.txt 
+#    nvidia-smi -q -d CLOCK >> clock_log.txt
+#    sleep 5 
+#done
 
